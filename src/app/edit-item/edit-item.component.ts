4544191e2806c2f4759da4eb3f1f83f1a1bf5b16
lavrinec <ml9987@student.uni-lj.si>
2020-01-31 13:49:42 +0100

    small fixes

--- src/app/edit-item/edit-item.component.ts
+++ src/app/edit-item/edit-item.component.ts
@@ -1,17 +1,37 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-edit-item',
   templateUrl: './edit-item.component.html',
-  styleUrls: ['./edit-item.component.css']
+  styleUrls: ['./edit-item.component.css'],
 })
-export class EditItemComponent implements OnInit {
+export class EditItemComponent implements OnInit, OnDestroy {
+  private _item;
+  @Input() set item(item) {
+    this._item = item;
+    this.form.setValue(this.item);
+  }
+  get item() {
+    return this._item;
+  }
+  @Output() itemChange;
+  form: FormGroup;
+  private onDestroy$ = new Subject();
 
-  @Input() item;
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      company: ['', Validators.compose([Validators.required])],
+      contact: '',
+    });
+    this.itemChange = this.form.valueChanges;
   }
 
+  ngOnInit() {}
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
 }
